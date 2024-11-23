1e001dd3115fa57cbaedfbf34e0a1f14ba482b69
skpeterson <skpet@unc.edu>
2024-11-23 16:36:18 -0500

    update dockerfile with scikit-learn and pandas

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,7 @@
-docker run -v $(pwd):/home/rstudio/work -p 8787:8787 -p 8888:8888 -t 611
+#!/bin/bash
+
+docker run -it --rm \
+       	-v $(pwd):/home/rstudio/work \
+       	-p 8787:8787 \
+	-p 8888:8888 \
+	-t 611
